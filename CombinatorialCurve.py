3f3baddf27f2ab0f9b910914321a31d6cb8957ad
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-20 08:04:15 -0600

    Add a lot of comments and fix some typos

--- CombinatorialCurve.py
+++ CombinatorialCurve.py
@@ -1,9 +1,11 @@
 # A vertex has a name and non-negative genus
 class vertex(object):
+    # name_ should be a string identifier - only unique if the user is careful (or lucky) to make it so
+    # genus_ should be a non-negative integer
     def __init__(self, name_, genus_):
+        # Don't allow negative genus!
         if genus_ < 0:
             raise ValueError("Genus must be non-negative")
-
         self.name = name_
         self._genus = genus_
 
@@ -11,62 +13,78 @@ class vertex(object):
     def genus(self):
         return self._genus
 
+    # Control how the genus property is set
+    # genus_ should be a non-negative integer
     @genus.setter
     def genus(self, genus_):
+        # Don't allow negative genus!
         if genus_ < 0:
             raise ValueError("Genus must be non-negative.")
         self._genus = genus_
 
 # An edge has a name, non-negative length, and endpoints
 class edge(object):
-    def __init__(self, name_, length_, vec1_, vec2_):
+    # name_ should be a string identifier - only unique if the user is careful (or lucky) to make it so
+    # length_ should be a non-negative double
+    # vert1_ should be a vertex
+    # vert2_ should be a vertex
+    def __init__(self, name_, length_, vert1_, vert2_):
         self.name = name_
 
+        # Don't allow negative lengths!
         if length_ < 0.0:
             raise ValueError("Length must be non-negative.")
         self._length = length_
 
-        #Endpoints
-        self.vec1 = vec1_
-        self.vec2 = vec2_
+        # Distinguished endpoints to help identify self loops
+        self.vert1 = vert1_
+        self.vert2 = vert2_
 
     @property
     def length(self):
         return self._length
 
+    # Control how the length property is set
+    # length_ should be a non-negative double
     @length.setter
     def setLength(self, length_):
+        # Don't allow negative lengths!
         if length_ < 0.0:
             raise ValueError("Length must be non-negative.")
         self._length = length_
 
+    # The set of vertices is a read only property computed upon access
     @property
     def vertices(self):
-        return {self.vec1, self.vec2}
+        return {self.vert1, self.vert2}
 
 #A leg has a name and root
 class leg(object):
+    # name_ should be a string identifier - only unique if the user is careful (or lucky) to make it so
+    # root_ should be a vertex
     def __init__(self, name_, root_):
         self.name = name_
         self.root = root_
 
+    # The set of vertices is a read only property computed upon access
     @property
     def vertices(self):
         return {self.root}
 
 # A Combinatorial Tropical Curve has a name, set of edges, and set of legs
 class CombCurve(object):
-
+    # name_ should be a string identifier - only unique if the user is careful (or lucky) to make it so
     def __init__(self, name_):
         self.name = name_
         self._edges = {}
         self._legs = {}
 
-
     @property
     def edges(self):
         return self._edges
 
+    # Control how edges are set
+    # edges_ should be a set of edges
     @edges.setter
     def edges(self, edges_):
         self._edges = edges_
@@ -75,12 +93,17 @@ class CombCurve(object):
     def legs(self):
         return self._legs
 
+    # Control how legs are set
+    # legs_ should be a set of legs
     @legs.setter
     def legs(self, legs_):
         self._legs = legs_
 
+    # The set of vertices is a read only property computed upon access
+    # It is the collection of vertices that are endpoints of edges or roots of legs
     @property
     def vertices(self):
+        # Flatmap self.edges with the function e => e.vertices
         unflattened_vertex_list = [e.vertices for e in self.edges] + [l.vertices for l in self.legs]
         flattened_vertex_list = []
         for sublist in unflattened_vertex_list:
@@ -88,65 +111,81 @@ class CombCurve(object):
                 flattened_vertex_list.append(vertex)
         return set(flattened_vertex_list)
 
+    # The number of vertices is a read only property computed upon access
+    # It is the number of vertices
     @property
     def vertexNumber(self):
         return len(self.vertices)
 
+    # The number of edges is a read only property computed upon access
+    # It is the number of edges
     @property
     def edgeNumber(self):
         return len(self.edges)
 
-
     def showNumbers(self):
         print "Number of Vertices: ", self.vertexNumber, " Number of Edges: ", self.edgeNumber
 
+    # Prints the names of vertices
     def showVertices(self):
         print [v.name for v in self.vertices]
 
+    # Prints the names of edges
     def showEdges(self):
         print [e.name for e in self.edges]
 
+    # Prints the names of legs
     def showLegs(self):
         print [l.name for l in self.legs]
 
+    # Returns the degree of vertex v accounting for legs and self loops
     def degree(self, v):
-        return sum(1 for e in self.edges if e.vec1 == v) + sum(1 for e in self.edges if e.vec2 == v) + sum(1 for l in self.legs if l.root == v)
+        return sum(1 for e in self.edges if e.vert1 == v) + sum(1 for e in self.edges if e.vert2 == v) + sum(1 for l in self.legs if l.root == v)
 
+    # The Betti number is a read only property computed upon access
     @property
     def bettiNumber(self):
         return self.edgeNumber - self.vertexNumber + 1
 
+    # Genus is a read only property computed upon access
+    # TODO: Consider caching this - slower for large graphs
     @property
     def genus(self):
         return self.bettiNumber + sum([v.genus for v in self.vertices])
 
 
 class StrictPiecewiseLinearFunction(object):
+    # domain_ should be a CombCurve representing the domain of the function
+    # functionValues_ should be a dictionary with vertex/leg keys and non-negative double values
     def __init__(self, domain_, functionValues_):
         self._domain = domain_
         self._functionValues = functionValues_
         self.assertIsAffineLinear()
 
+    # Make the domain read only
     @property
     def domain(self):
         return self._domain 
 
+    # Make the function read only
     @property
     def functionValues(self):
         return self._functionValues
 
     def assertIsAffineLinear(self):
         # Assert Non-Negativity at every iteration of the loop!
-        
         for i in self.functionValues.values():
             assert i >= 0.0
-
         for v in self.domain.vertices:
+            # Ensure that every vertex is in the domain of the function
             assert v in self.functionValues
         for l in self.domain.legs:
+            # Ensure that every leg is in the domain of the function
             assert l in self.functionValues
+            # Ensure that each m(l) is an integer
             assert self.functionValues[l].is_integer()
         for e in self.domain.edges:
             if e.length > 0.0:
-                assert ((self.functionValues[e.vec1] - self.functionValues[e.vec2]) / e.length).is_integer()
+                # Ensure the function has integer slope
+                assert ((self.functionValues[e.vert1] - self.functionValues[e.vert2]) / e.length).is_integer()
     
