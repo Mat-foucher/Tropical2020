7691287b7c2635c7c6c8aef1b5e7f541df1d44fe
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-20 07:35:42 -0600

    Move tests and examples to their own file

--- CombinatorialCurve.py
+++ CombinatorialCurve.py
@@ -150,35 +150,3 @@ class StrictPiecewiseLinearFunction(object):
             if e.length > 0.0:
                 assert ((self.functionValues[e.vec1] - self.functionValues[e.vec2]) / e.length).is_integer()
     
-
-
-
-    
-
-C = CombCurve("Example 3.5")
-v1 = vertex("v1", 0)
-v2 = vertex("v2", 0)
-v3 = vertex("v3", 1)
-e1 = edge("e1", 1.0, v1, v2)
-e2 = edge("e2", 1.0, v2, v3)
-e3 = edge("e3", 1.0, v1, v3)
-e4 = edge("e4", 1.0, v1, v1)
-l = leg("l", v1)
-
-C.edges = {e1, e2, e3, e4}
-C.legs = {l}
-
-dict = {v1: 0, v2: 0, v3: 0, l: 0}
-
-f = StrictPiecewiseLinearFunction(C, dict)
-
-assert C.vertices == {v1, v2, v3}
-
-assert C.degree(v1) == 5
-assert C.degree(v2) == 2
-assert C.degree(v3) == 2
-
-assert C.genus == 3
-assert C.bettiNumber == 2
-
-print "If you see this, then all previous assertations were true!"
