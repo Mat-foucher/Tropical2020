26a72136499f6288849a36ea92e0d5449b808e79
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-21 18:57:19 -0600

    Test getSpecialSupportPartition for the 9 element chain with 3 support blocks

--- tests.py
+++ tests.py
@@ -26,11 +26,11 @@ f = StrictPiecewiseLinearFunction(C, dict)
 
 s = f.getSpecialSupportPartition()
 
-print(s)
-
-for x in s:
-    for e in x:
-        print e.name
+#print(s)
+assert s == [{e1, e3, e4}]
+#for x in s:
+#    for e in x:
+#        print(e.name)
 
 assert f.functionValues[v1] == 1.0
 for v in C.vertices:
@@ -156,6 +156,47 @@ assert core_.isConnected
 assert core_.genus == C.genus
 assert core_.vertices == {v1, v3}
 
+
+
+
+C = CombCurve("Chain of 9 vertices")
+v1 = vertex("v1", 0)
+v2 = vertex("v2", 0)
+v3 = vertex("v3", 0)
+v4 = vertex("v4", 0)
+v5 = vertex("v5", 0)
+v6 = vertex("v6", 0)
+v7 = vertex("v7", 0)
+v8 = vertex("v8", 0)
+v9 = vertex("v9", 0)
+e1 = edge("e1", 1.0, v1, v2)
+e2 = edge("e2", 1.0, v2, v3)
+e3 = edge("e3", 1.0, v3, v4)
+e4 = edge("e4", 1.0, v4, v5)
+e5 = edge("e5", 1.0, v5, v6)
+e6 = edge("e6", 1.0, v6, v7)
+e7 = edge("e7", 1.0, v7, v8)
+e8 = edge("e8", 1.0, v8, v9)
+
+C.edges = {e1, e2, e3, e4, e5, e6, e7, e8}
+
+f = StrictPiecewiseLinearFunction(C, {v1: 0.0, v2: 1.0, v3: 0.0, v4: 1.0, v5: 0.0, v6: 0.0, v7: 1.0, v8: 1.0, v9: 0.0})
+
+
+#supportEdges, supportVertices = f.getSpecialSupport()
+#for e in supportEdges:
+#    print(e.name)
+#for v in supportVertices:
+#    print(v.name)
+s = f.getSpecialSupportPartition()
+#print(s)
+#for x in s:
+#    print("Next support block:")
+#    for e in x:
+#        print(e.name)
+
+# Python doesn't allow sets of mutable sets, so we convert these to sets of immutable sets before comparing
+assert {frozenset(block) for block in s} == {frozenset({e1, e2}), frozenset({e3, e4}), frozenset({e6, e7, e8})}
 
 
 print("If you see this, then all previous assertations were true!")
