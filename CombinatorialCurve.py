4357c3bcf81a23a4c210004b37f2df1f959fe518
Connor Meredith <connor.meredith0913@gmail.com>
2020-05-27 07:04:10 -0600

    Revise the simplifyNames function

--- CombinatorialCurve.py
+++ CombinatorialCurve.py
@@ -451,9 +451,9 @@ class CombCurve(object):
         for i in range(len(orderedVertices)):
             orderedVertices[i].name = "v" + str(i)
         for e in self.edges:
-            e.name = "edge: " + e.vert1.name + ", " + e.vert2.name
+            e.name = "edge(" + e.vert1.name + ", " + e.vert2.name + ")"
         for nextLeg in self.legs:
-            nextLeg.name = "leg: " + nextLeg.root.name
+            nextLeg.name = "leg(" + nextLeg.root.name + ")"
 
     def showNumbers(self):
         print("Number of Vertices: ", self.vertexNumber, " Number of Edges: ", self.edgeNumber)
