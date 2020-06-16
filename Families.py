e6c76e91d4999d83f5488afd3b3a45e2afa51b79
Connor Meredith <connor.meredith0913@gmail.com>
2020-06-16 16:47:42 -0600

    Set up compatibility code

--- Families.py
+++ Families.py
@@ -1,6 +1,7 @@
 
 class Family(object):
-    pass
+    def __init__(self):
+        self.morphisms = set()
 
 
 class FamilyMorphism(object):
@@ -12,5 +13,26 @@ class TropicalFamily(Family):
 
 
 class PLFFamily(object):
-    pass
+    # domain: Family
+    # functions: Dictionary[BasicFamily, SPLF]
+    def __init__(self, domain, functions):
+        assert isinstance(domain, Family)
+        assert isinstance(functions, dict)
+        for key in functions:
+            assert functions[key].domain == key
+
+        self.domain = domain
+        self.functions = functions
+
+        if not self.isWellDefined():
+            raise ValueError("The given functions are not compatible with each other.")
+
+    def morphismPreservesFunctions(self, morphism):
+        pass
+
+    def isWellDefined(self):
+        for morphism in self.domain.morphisms:
+            if not self.morphismPreservesFunctions(morphism):
+                return False
+        return True
 
