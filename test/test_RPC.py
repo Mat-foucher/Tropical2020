4710018855590239b6b95b24d6eec47f034190ff
Connor Meredith <connor.meredith0913@gmail.com>
2020-07-15 15:06:20 -0600

    (Temp) Put everything in test_* functions

--- test/test_RPC.py
+++ test/test_RPC.py
@@ -1,32 +1,34 @@
 from Tropical2020.basic_families.RPC import *
 import pdb
 
-M = Monoid()
-X = "x"
-Y = "y"
-M.addgen(X)
-M.addgen(Y)
-x = M.Element({X:1})
-y = M.Element({Y:1})
+def test_RPC():
 
-assert not M.eq(x,y)
+    M = Monoid()
+    X = "x"
+    Y = "y"
+    M.addgen(X)
+    M.addgen(Y)
+    x = M.Element({X:1})
+    y = M.Element({Y:1})
 
-M.addrel(x-y)
+    assert not M.eq(x,y)
 
-assert M.eq(x,y)
+    M.addrel(x-y)
 
-
-P = Monoid()
-P.addgen(1)
-P.addgen(2)
-P.addgen(3)
-x = P.Element({1:1})
-y = P.Element({2:1})
-z = P.Element({3:1})
-P.addrel(x + y - 2 * z)
+    assert M.eq(x,y)
 
 
-P.compute_dual()
+    P = Monoid()
+    P.addgen(1)
+    P.addgen(2)
+    P.addgen(3)
+    x = P.Element({1:1})
+    y = P.Element({2:1})
+    z = P.Element({3:1})
+    P.addrel(x + y - 2 * z)
 
 
-w = y - x
+    P.compute_dual()
+
+
+    w = y - x
