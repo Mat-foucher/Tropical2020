e4d35d94560db5b6b87072984e5d74a258020a90
Connor Meredith <connor.meredith0913@gmail.com>
2020-07-21 16:54:11 -0600

    Manually list requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Mat-foucher/Tropical2020",
-    packages=setuptools.find_packages(),
+    packages=['numpy', 'pytest'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
