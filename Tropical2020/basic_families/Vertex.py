b30af3e86d8d921f10c99fd4bf943927a34618d7
Connor Meredith <connor.meredith0913@gmail.com>
2020-07-31 13:25:28 -0600

    Add type annotations

--- Tropical2020/basic_families/Vertex.py
+++ Tropical2020/basic_families/Vertex.py
@@ -1,21 +1,21 @@
 class Vertex(object):
     # name_ should be a string identifier - only unique if the user is careful (or lucky) to make it so
     # genus_ should be a non-negative integer
-    def __init__(self, name_, genus_):
+    def __init__(self, name_: str, genus_: int) -> None:
         # Don't allow negative genus!
         if genus_ < 0:
             raise ValueError("Genus must be non-negative")
-        self.name = name_
-        self._genus = genus_
+        self.name: str = name_
+        self._genus: int = genus_
 
     @property
-    def genus(self):
+    def genus(self) -> int:
         return self._genus
 
     # Control how the genus property is set
     # genus_ should be a non-negative integer
     @genus.setter
-    def genus(self, genus_):
+    def genus(self, genus_: int) -> None:
         # Don't allow negative genus!
         if genus_ < 0:
             raise ValueError("Genus must be non-negative.")
