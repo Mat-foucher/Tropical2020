eb207e313b92c5d191ea40da46e1cfbf1d903200
Mathieu Foucher <mafo8363@colorado.edu>
2020-08-23 09:47:50 -0600

    Updated FamilyList as a class in FamilyList.py

--- Tropical2020/basic_families/FamilyList.py
+++ Tropical2020/basic_families/FamilyList.py
@@ -2,8 +2,21 @@ from .BasicFamily import *
 from .RPC import *
 
 class FamilyList(object):
-    def __init__(self, )
+    def __init__(self, _BasicFamily, _Monoid, _LengthDictionary, _LegLengthVector):
+        self.BasicFamily_ = _BasicFamily
+        self.Monoid_ = _Monoid
+        self.LengthDictionary = _LengthDictionary
+        self.LegLengthVector = _LegLengthVector
 
+    @property
+    def BasicFamily(self):
+        return self.BasicFamily_
 
+    @property
+    def Monoid(self):
+        return self.Monoid_
+
+    def AssignLengthsToEdges(self, dict):
+        
 
 
